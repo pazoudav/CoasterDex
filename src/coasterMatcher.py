9a90ae6e843172a1eb2b37aa985efe89e04730c8
pazoudav <pazoutdavid@gmail.com>
2023-12-05 18:25:25 +0000

    matcher first commit

--- src/coasterMatcher.py
+++ src/coasterMatcher.py
@@ -1,6 +1,43 @@
+import cv2 as cv
+from matcher.encoder import Encoder, VLAD
+from matcher.featureExtractor import FeaturesExtractor, SIFT
+from matcher.helper import *
+
+    
+def build_codebook_from_coco():
+    fe = SIFT()
+    descriptors = []
+    
+    print('loading descriptors')
+    for file in folder_iterator('dataset/coco128'):
+        image, _ = open_image(file)
+        _, des = fe.extract(image)
+        descriptors.append(des)
+    print('descriptors loaded')    
+    
+    enc = VLAD().load()
+    enc.build_codebook(descriptors).save()
+    print('codebook build')   
+    
+    
+class Lookup:
+    ...
 
 
 class CoasterMatcher:
-    def match(self, img, **kwargs):
+    def __init__(self):
+        self.featureExtractor : FeaturesExtractor = SIFT()
+        self.encoder : Encoder = VLAD().load()
+        self.lookup : Lookup = Lookup()  
+        
+    def match(self, image, **kwargs):
         best_matches = None
+        key_points, descriptors = self.featureExtractor.extract()
         return best_matches, 
+    
+    
+    
+if __name__ == '__main__':
+    build_codebook_from_coco()
+
+    
---
+++ src/matcher/encoder.py
@@ -0,0 +1,40 @@
+from matcher.vlad import VLAD as VLAD_
+from joblib import dump, load
+
+class Encoder:
+    def __init__(self) -> None:
+        self.codebook = None
+    
+    def load_codebook(self, path):
+        self = load(path)
+        return self
+        
+    def build_codebook(self, descriptors):
+        return self
+        
+    def encode(self, descriptors):
+        ...
+
+    
+    
+class VLAD(Encoder):
+    def __init__(self) -> None:
+        super().__init__()
+        self.codebook = VLAD_()
+        
+    def build_codebook(self, descriptors):
+        print('building codebook...')
+        self.codebook.fit(descriptors)
+        return self
+    
+    def save(self):
+        dump(self, 'src/matcher/VLAD.enc')
+        return self
+    
+    def load(self):
+        self = load('src/matcher/VLAD.enc')
+        return self
+        
+    def encode(self, descriptors):
+        return self.codebook.transform(descriptors)
+        
